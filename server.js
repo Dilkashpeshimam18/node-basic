5c44f64487b5c41d510e5708cb5992e1d65efada
Dilkash Peshimam <dilkashpeshimam@gmail.com>
2023-02-25 10:28:50 -0800

    showing data from file

--- server.js
+++ server.js
@@ -1,30 +1,51 @@
 const http = require('http')
-
+const fs=require('fs')
 
 const server = http.createServer((req, res) => {
     const url = req.url
+   const method=req.method
+    if(url==='/'){
+     constdata=fs.readFile('message.txt',{encoding:'utf-8'},(err,data)=>{
+  if(err){
+    console.log(err)
+  }
+  res.write("<html>");
+  res.write('<head><title>Enter message</title></head>')
+res.write(`<body>${data}</body>`)
+res.write("</html>")
+res.write('<body><form action="/message" method="POST"><input type="text" name="message" /><button>Send</button></form></body>')
+return res.end()
 
-    if (url == '/node') {
-        res.write('<html>')
-        res.write('<head><title>First node</title></head>')
-        res.write('<body><h1>Welcome to my node js project</h1></body>')
-        res.write('</html>')
-        return res.end()
+console.log("this is data",data)
 
-    } else if (url == '/home') {
-        res.write('<html>')
-        res.write('<head><title>First node</title></head>')
-        res.write('<body><h1>Welcome Home</h1></body>')
-        res.write('</html>')
-        return res.end()
-    } else if (url == '/about') {
-        res.write('<html>')
-        res.write('<head><title>First node</title></head>')
-        res.write('<body><h1>Welcome About</h1></body>')
-        res.write('</html>')
-        return res.end()
+     })
+
+ 
     }
-    res.setHeader('Content-Type', 'text/html')
+
+    if(url==='/message' && method==="POST"){
+        const body=[]
+        req.on('data',(chunk)=>{
+           console.log(chunk)
+           body.push(chunk)
+        })
+        req.on('end',()=>{
+            const parsedBody=Buffer.concat(body).toString()
+            console.log(parsedBody)
+            const message=parsedBody.split('=')[1]
+            fs.writeFile('message.txt',message,(err)=>{
+                console.log(err)
+        
+            })
+  
+        })
+        res.statusCode=302;
+        res.setHeader('Location','/')
+         return res.end();
+     
+    }
+
+   res.setHeader('Content-Type','text/html')
     res.write('<html>')
     res.write('<head><title>First node</title></head>')
     res.write('<body><h1>Hello</h1></body>')
