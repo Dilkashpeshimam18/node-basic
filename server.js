360fe26fa950547ae70c90d5a041f319953c8693
Dilkash Peshimam <dilkashpeshimam@gmail.com>
2023-02-28 00:51:01 -0800

    added route middleware & parsing body

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
-//creating server & middleware using express
+//creating server, route middleware parsing the body using express
 const express=require('express')
+const bodyParser=require('body-parser')
 
 const app=express()
 
-//it will execute on every incoming request
-app.use((req,res,next)=>{
-  console.log('Inside middleware')
-  next(); //allow request to continue to next middleware line!
+//parsing body
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+//route handling middleware
+app.use('/add-product',(req,res,next)=>{
+  res.send('<html><body><form action="/product" method="POST"><input type="text" name="title" /><input type="number" name="size" />  <button type="submit">Add product</button></form></body></html>')
 })
 
-app.use((req,res,next)=>{
-    console.log('Inside another middleware')
+app.use('/product',(req,res,next)=>{
+  console.log(req.body)
+  res.redirect('/')
+})
+app.use('/',(req,res,next)=>{
     res.send('<h1>Hello world</h1>')
   })
 
