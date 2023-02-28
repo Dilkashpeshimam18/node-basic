5de508fb76c4b6c3b8a9534f646f7956d429bf5a
Dilkash Peshimam <dilkashpeshimam@gmail.com>
2023-02-28 03:42:09 -0800

    created separate routes file & router using express router

--- server.js
+++ server.js
@@ -3,22 +3,20 @@ const express=require('express')
 const bodyParser=require('body-parser')
 
 const app=express()
-
+const adminRouter=require('./routes/admin')
+const shopRouter=require('./routes/shop')
 //parsing body
 
 app.use(bodyParser.urlencoded({extended:false}))
 
 //route handling middleware
-app.use('/add-product',(req,res,next)=>{
-  res.send('<html><body><form action="/product" method="POST"><input type="text" name="title" /><input type="number" name="size" />  <button type="submit">Add product</button></form></body></html>')
+
+app.use(adminRouter)
+app.use(shopRouter)
+
+app.use((req,res,next)=>{
+  res.status(404).send('<h1>Page not found</h1>')
 })
 
-app.use('/product',(req,res,next)=>{
-  console.log(req.body)
-  res.redirect('/')
-})
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Hello world</h1>')
-  })
 
 app.listen(4000)
