f5b74785330d6dd00f735541e5eb552e6f926df4
Dilkash Peshimam <dilkashpeshimam@gmail.com>
2023-03-03 05:25:38 -0800

    created model for storing & retrieving data from file

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,5 @@
 const path=require('path')
+const Product=require('../models/product')
 //controller that only work with product logic
 
 
@@ -9,9 +10,14 @@ exports.addProduct=(req,res,next)=>{
 
 exports.postAddProduct=(req,res,next)=>{
     console.log(req.body)
+   const product=new Product(req.body.title,req.body.size)
+   product.save()
     res.redirect('/')
   }
 
   exports.getProducts=(req,res,next)=>{
+    const product=Product.fetchAll((products)=>{
+     console.log(`All product ${products}`)
+    })
     res.sendFile(path.join(__dirname,'../','views','shop.html'))
   }
